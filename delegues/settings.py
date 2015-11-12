b13031b004f3879c52a70ec3f3d270e7d5043536
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-11-12 19:37:51 +0100

    Add basic project + DjangoBB folder

--- delegues/settings.py
+++ delegues/settings.py
@@ -14,13 +14,13 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'at*=#y#4%w=@0@gia#l7)o)605!%m&+4w_z9od-2)hbjb1bq!('
+SECRET_KEY = os.getenv('DELEGUES_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'djangobb_forum'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
