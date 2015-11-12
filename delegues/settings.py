0e9a2e635d9dd145001ce82463e11b2ddae27303
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-11-12 23:17:20 +0100

    Use flat theme (last one ʘ‿ʘ)

--- delegues/settings.py
+++ delegues/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
+    'flat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +109,7 @@ HAYSTACK_CONNECTIONS = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
