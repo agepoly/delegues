d1e58cb1ee896545e4d60451ce8334899fd33656
Louis <louis.merlin@epfl.ch>
2015-11-14 16:29:17 +0100

    Template cloned in delegues :D

--- delegues/settings.py
+++ delegues/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'delegues.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'delegues/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
