54c8084ece687035a58d68dd2a587e651368c012
Louis Merlin <louis.merlin@epfl.ch>
2015-11-16 22:26:44 +0100

    Added login (/login) and padding for submit button

--- delegues/urls.py
+++ delegues/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('djangobb_forum.urls', namespace='djangobb'))
+    url(r'', include('djangobb_forum.urls', namespace='djangobb')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
